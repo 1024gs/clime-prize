8f9f7bbaf79ab31ab8a35799f4f4a3ee1378e074
Sergiu Ghenciu <1024gs@gmail.com>
2020-06-04 19:36:58 +0100

    Move prize-card to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PrizeCardComponent } from './prize-card/prize-card.component';
+import { PrizeCardComponent } from './components/prize-card/prize-card.component';
 
 @NgModule({
   declarations: [
