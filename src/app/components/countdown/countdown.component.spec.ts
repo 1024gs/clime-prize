1b7d22ceb620dbabd98ab888b13ce1204ff6b743
Sergiu Ghenciu <1024gs@gmail.com>
2020-06-05 21:31:49 +0100

    Unit test countdown component

--- src/app/components/countdown/countdown.component.spec.ts
+++ src/app/components/countdown/countdown.component.spec.ts
@@ -1,4 +1,12 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  tick,
+  discardPeriodicTasks,
+  fakeAsync,
+} from '@angular/core/testing';
+import { SecondsToTimePipe } from '../../pipes/seconds-to-time.pipe';
 
 import { CountdownComponent } from './countdown.component';
 
@@ -8,17 +16,41 @@ describe('CountdownComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [CountdownComponent],
+      declarations: [CountdownComponent, SecondsToTimePipe],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CountdownComponent);
     component = fixture.componentInstance;
+    component.seconds = 25;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should countdown', fakeAsync(() => {
+    component.seconds = 5;
+
+    component.ngOnInit();
+    tick(1000);
+    expect(component.seconds).toEqual(4);
+
+    tick(1000);
+    expect(component.seconds).toEqual(3);
+
+    discardPeriodicTasks();
+  }));
+
+  it('should stop countdown when reacing 0', fakeAsync(() => {
+    component.seconds = 1;
+
+    component.ngOnInit();
+    tick(2000);
+
+    expect(component.seconds).toEqual(0);
+    discardPeriodicTasks();
+  }));
 });
