5e8139f210c0a7444d7d4d7d1189ed482e2b3084
Sergiu Ghenciu <1024gs@gmail.com>
2020-06-05 21:56:23 +0100

    Unit test the prize-card component

--- src/app/components/prize-card/prize-card.component.spec.ts
+++ src/app/components/prize-card/prize-card.component.spec.ts
@@ -15,10 +15,29 @@ describe('PrizeCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PrizeCardComponent);
     component = fixture.componentInstance;
+    component.prize = {
+      amount: 50,
+      prize: 'Free Spins',
+      game: 'Gamix',
+      countdown: 25,
+    };
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#claim() sets the prize claimed prop to true', () => {
+    expect(component.prize.claimed).not.toBeTruthy();
+    component.claim();
+    expect(component.prize.claimed).toBe(true);
+  });
+
+  it('#onTimeout() sets the prize countdown prop to 0', () => {
+    expect(component.prize.countdown).not.toBe(0);
+    component.onTimeout();
+    expect(component.prize.countdown).toBe(0);
   });
 });
