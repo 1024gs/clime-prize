1c1541942550a2a7db8dd7cef56b9f5a4d6be436
Sergiu Ghenciu <1024gs@gmail.com>
2020-06-05 18:19:13 +0100

    Add claimed card

--- src/app/components/prize-card/prize-card.component.ts
+++ src/app/components/prize-card/prize-card.component.ts
@@ -2,29 +2,57 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-prize-card',
-  template: `<div class="card text-center">
+  template: `<div
+    class="card text-center"
+    [ngClass]="{ claimed: prize.claimed }"
+  >
     <div class="card-body">
       <h5 class="card-title">
-        50 Free Spins <br />
-        on Gemix
+        <span *ngIf="!prize.claimed"
+          >{{ prize.amount }} {{ prize.prize }} <br />
+          on {{ prize.game }}</span
+        >
+        <span *ngIf="prize.claimed"
+          >You've {{ prize.amount }} {{ prize.prize }} to use now</span
+        >
       </h5>
-      <p class="card-text">
+      <p class="card-text" *ngIf="!prize.claimed">
         <app-countdown
-          [seconds]="5"
-          (complete)="onComplete()"
+          [seconds]="prize.countdown"
+          (complete)="onTimeout()"
         ></app-countdown>
       </p>
-      <button type="button" class="btn btn-primary">Claim now</button>
+      <button
+        type="button"
+        class="btn btn-primary"
+        [disabled]="prize.countdown === 0 || prize.claimed"
+        (click)="claim()"
+      >
+        <span *ngIf="!prize.claimed">Claim now</span>
+        <img *ngIf="prize.claimed" src="assets/icon-tick.svg" alt="tick" />
+      </button>
     </div>
   </div>`,
   styleUrls: ['./prize-card.component.sass'],
 })
 export class PrizeCardComponent implements OnInit {
+  prize = {
+    amount: 50,
+    prize: 'Free Spins',
+    game: 'Gamix',
+    countdown: 5,
+    claimed: false,
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
 
-  onComplete() {
-    console.log('oncomplete');
+  claim() {
+    this.prize.claimed = true;
+  }
+
+  onTimeout() {
+    this.prize.countdown = 0;
   }
 }
