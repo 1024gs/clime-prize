c9985eef030c3d4fbd5e8493b919a91afe76b088
Sergiu Ghenciu <1024gs@gmail.com>
2020-06-05 20:50:59 +0100

    Add is-loading component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { PrizeCardComponent } from './components/prize-card/prize-card.component
 import { CountdownComponent } from './components/countdown/countdown.component';
 import { SecondsToTimePipe } from './pipes/seconds-to-time.pipe';
 import { HttpClientModule } from '@angular/common/http';
+import { IsLoadingComponent } from './components/is-loading/is-loading.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrizeCardComponent,
     CountdownComponent,
-    SecondsToTimePipe
+    SecondsToTimePipe,
+    IsLoadingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/is-loading/is-loading.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-is-loading',
+  template: `<div class="is-loading">Loading...</div>`,
+  styleUrls: ['./is-loading.component.sass'],
+})
+export class IsLoadingComponent {}
