29085514bf2695ae0a4a8f8e736e8d0c891f7312
Sergiu Ghenciu <1024gs@gmail.com>
2020-06-05 18:27:40 +0100

    Move the template to html file

--- src/app/components/prize-card/prize-card.component.ts
+++ src/app/components/prize-card/prize-card.component.ts
@@ -2,37 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-prize-card',
-  template: `<div
-    class="card text-center"
-    [ngClass]="{ claimed: prize.claimed }"
-  >
-    <div class="card-body">
-      <h5 class="card-title">
-        <span *ngIf="!prize.claimed"
-          >{{ prize.amount }} {{ prize.prize }} <br />
-          on {{ prize.game }}</span
-        >
-        <span *ngIf="prize.claimed"
-          >You've {{ prize.amount }} {{ prize.prize }} to use now</span
-        >
-      </h5>
-      <p class="card-text" *ngIf="!prize.claimed">
-        <app-countdown
-          [seconds]="prize.countdown"
-          (complete)="onTimeout()"
-        ></app-countdown>
-      </p>
-      <button
-        type="button"
-        class="btn btn-primary"
-        [disabled]="prize.countdown === 0 || prize.claimed"
-        (click)="claim()"
-      >
-        <span *ngIf="!prize.claimed">Claim now</span>
-        <img *ngIf="prize.claimed" src="assets/icon-tick.svg" alt="tick" />
-      </button>
-    </div>
-  </div>`,
+  templateUrl: './prize-card.component.html',
   styleUrls: ['./prize-card.component.sass'],
 })
 export class PrizeCardComponent implements OnInit {
